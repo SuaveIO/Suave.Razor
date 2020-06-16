0384e721df36487b9bb858006f92d03d031a56bc
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2020-06-16 14:12:51 -0400

    Suave.Razor for netcore

---
+++ Suave.Razor.Tests/Program.fs
@@ -0,0 +1,14 @@
+﻿open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Razor
+
+type Model = { bar : string }
+
+let testApp = 
+  choose [ 
+    path "/test1" >=> razor "test1" { bar = "World"}
+    path "/test2" >=> razor "test2" null
+    RequestErrors.NOT_FOUND "File not found."]
+
+startWebServer defaultConfig testApp
